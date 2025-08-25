302de5cda467ce31634a12a2342c5991932c0164
dedun1 <eyadkhalil51@gmail.com>
2025-08-25 17:01:18 +0300

    added the file allowed file types , files max size and file default chunk size

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,13 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Json
 
 class Settings(BaseSettings):
-    
-    APP_NAME:str ="mini-RAG"
-    APP_VERSION:str  = "1.0.0"
-    OPENAI_API_KEY:str | None = None
-        
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-        
-def get_settings():
+    APP_NAME: str = "mini-RAG"
+    APP_VERSION: str = "1.0.0"
+    OPENAI_API_KEY: str | None = None
+
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
+
+    model_config = SettingsConfigDict(env_file=".env",env_file_encoding="utf-8",)
+
+def get_settings() -> "Settings":
     return Settings()
-        
