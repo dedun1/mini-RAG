0919a9f0a206bf2acfda48784a89a6717688838b
dedun1 <eyadkhalil51@gmail.com>
2025-08-25 17:13:52 +0300

    return uploaded file id

--- src/routes/data.py
+++ src/routes/data.py
@@ -30,7 +30,9 @@ async def upload_data(
         )
 
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = data_controller.generate_unique_filename(orig_file_name=file.filename,project_id=project_id)
+    file_path,file_id = data_controller.generate_unique_filepath(
+         orig_file_name=file.filename,
+         project_id=project_id)
     
     try:
         async with aiofiles.open(file_path, "wb") as f:
@@ -45,5 +47,6 @@ async def upload_data(
         )
         
     return JSONResponse(
-        content={"signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value}
+        content={"signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                 "file_id":file_id}
     )
