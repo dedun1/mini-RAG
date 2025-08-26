9dd50a0178ae85be32ee3376c021588da77f605b
eyad <eyadkhalil51@gmail.com>
2025-08-26 14:37:53 +0300

    added processcontroller for new route process and added pydantic model for validation

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, UploadFile, File, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController , ProcessController
 import aiofiles, os
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
 
 logger= logging.getLogger('uvicorn.error')
 
@@ -50,3 +51,34 @@ async def upload_data(
         content={"signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
                  "file_id":file_id}
     )
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id:str,process_request:ProcessRequest):
+
+    file_id= process_request.file_id
+
+    chunk_size=process_request.chunk_size
+
+    overlap_size=process_request.overlap_size
+
+    process_controller= ProcessController(project_id=project_id)
+
+    file_content= process_controller.get_file_content(file_id=file_id)
+
+    file_chunks= process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks)== 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal":ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+    
+    return file_chunks
+
+
